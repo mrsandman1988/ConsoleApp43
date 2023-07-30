9af08e4f437e70f08b490fcd3f2f8847efe53d1c
sraphunanyan <User@ADMIN>
2023-07-30 20:13:01 +0400

    Add project files.

---
+++ ConsoleApp43/Person.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp43
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DOB { get; set; }
+        public string ClothesColor { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            if(obj == null)
+            {
+                return false;
+            }
+
+            Person person = obj as Person;
+            if(person != null)
+            {
+             return   person.FirstName == this.FirstName
+                    && person.LastName == this.LastName
+                    && person.DOB == this.DOB;
+            }
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            
+            int hashCode = 0;
+            int start = 17;
+            hashCode = start + this.FirstName.GetHashCode();
+            hashCode += this.LastName.GetHashCode();
+            hashCode += this.DOB.GetHashCode();
+            return hashCode;
+        }
+
+
+    }
+}
---
+++ ConsoleApp43/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ConsoleApp43
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 3, 2, 4 };
+            var newCollection = numbers.Distinct().ToList();
+            
+            var dob = DateTime.Now.AddYears(-25);
+            Person person1 = new Person()
+            {
+                FirstName= "Test",
+                LastName= "Test",
+                DOB = dob,
+                ClothesColor = "Red"
+            };
+            Person person2 = new Person()
+            {
+                FirstName = "Test",
+                LastName = "Test",
+                DOB = dob,
+                ClothesColor = "Red"
+            };
+           
+            var hash1 = person1.FirstName.GetHashCode();
+            var hash2 = person1.LastName.GetHashCode();
+            var hash3 = person1.DOB.GetHashCode();
+            Console.WriteLine($"{hash1} {hash2} {hash3}");
+        }
+    }
+}
